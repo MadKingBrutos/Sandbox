876e38777f06fa5d6776fa7d05a0210602a920f1
MadKingBrutos <kieran.bruton@my.jcu.edu.au>
2018-03-16 09:32:39 +1000

    Add name printing

--- odd_name.py
+++ odd_name.py
@@ -1,1 +1,9 @@
 """ Kieran "MadKingBrutos" Bruton """
+UserName = input("Please enter your name.")
+output = ""
+while UserName == "":
+    UserName = input("Invalid name, please try again.")
+for i in range(len(UserName)):
+    if i % 2 == 0:
+        output += UserName[i]
+print(output)
